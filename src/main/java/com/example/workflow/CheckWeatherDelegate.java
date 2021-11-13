921390220cba9fac896db9b619b5260f09f678eb
Chingiz-Kich <chingizik093@gmail.com>
2021-11-13 22:47:02 +0600

    Comunda tutorial

---
+++ src/main/java/com/example/workflow/CheckWeatherDelegate.java
@@ -0,0 +1,18 @@
+package com.example.workflow;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import java.util.Random;
+
+public class CheckWeatherDelegate implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+
+        Random random = new Random();
+
+        delegateExecution.setVariable("name", "Chingiz");
+        delegateExecution.setVariable("weatherOk", random.nextBoolean() );
+
+    }
+}
